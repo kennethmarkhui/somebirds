c08bdbaa2c126bbdc5682c3d0637c58b0b332c76
Wisely <wiselychung96@gmail.com>
2022-11-11 16:39:14 +0800

    added resusable card.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
 import Footer from "../components/footer";
+import Caroussel_card from "../components/caroussel-card";
 
 export default function Home() {
   return (
     <div>
+      <Caroussel_card
+        image="yes"
+        name="Cold Weather Run"
+        description="water-repellant running shoes so you can clock miles rain or shine or both"
+      />
       homepage
       <Footer />
     </div>
