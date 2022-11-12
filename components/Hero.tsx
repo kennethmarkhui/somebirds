118608e3d3edd7add343c5e43b92c00d5d0aa82a
Wisely <wiselychung96@gmail.com>
2022-11-12 13:34:24 +0800

    updated hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,18 +1,21 @@
 import Button from "./button";
+
 const Hero = () => {
   return (
-    <section className="min-h-screen flex flex-col justify-center items-center bg-black lg:flex-row gap-2 px-4">
-      <div className="flex flex-col items-center text-center gap-2 lg:flex-1 px-8">
-        <h2 className="text-white font-bold md:text-2xl">
-          Walks In A Winter Wonderland? Check.
-        </h2>
-        <p className="text-sm text-white md:text-base">
-          Stay warm and dry with the weather-ready Mizzle Collection.
-        </p>
-      </div>
-      <div className="flex">
-        <Button variant="fill">SHOP MEN</Button>
-        <Button variant="fill">SHOP WOMEN</Button>
+    <section className="min-h-screen bg-black bg-cover bg-center lg:flex-row gap-2 px-4 bg-[url('../public/pokemon_itzy.jpg')]">
+      <div className="absolute inset-x-1/3 inset-y-2/3">
+        <div className="">
+          <h2 className="text-white font-bold text-4xl py-4">
+            Walks In A Winter Wonderland? Check.
+          </h2>
+          <p className=" text-white py-4 text-xl">
+            Stay warm and dry with the weather-ready Mizzle Collection.
+          </p>
+        </div>
+        <div className="flex py-3 gap-4">
+          <Button variant="fill">SHOP MEN</Button>
+          <Button variant="fill">SHOP WOMEN</Button>
+        </div>
       </div>
     </section>
   );
