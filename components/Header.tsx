be54e1af6279b597c172f67c8e592193dc34885a
Wisely <wiselychung96@gmail.com>
2022-11-22 15:29:47 +0800

    updated header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Link from "next/link";
 import {
   HiOutlineMenu,
@@ -6,14 +7,28 @@ import {
   HiOutlineQuestionMarkCircle,
   HiOutlineShoppingCart,
 } from "react-icons/hi";
+import { Tab } from "@headlessui/react";
+import HeaderDropdown from "./HeaderDropdown";
 
 const navLinks = ["men", "women", "kids"];
 
+interface INavItem {
+  idx: number;
+  name: string;
+}
+const NavItems: INavItem[] = [
+  {
+    idx: 0,
+    name: "Fluff Collector",
+  },
+];
+
 const Header = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
   return (
     <header className="flex fixed top-0 inset-x-0 justify-between items-center bg-white px-8 py-2 z-10">
       <HiOutlineMenu size="2em" className="cursor-pointer md:hidden" />
-
+      {/* TABLIST */}
       <ul className="hidden md:flex gap-4">
         {navLinks.map((navLink) => (
           <li key={navLink}>
@@ -24,6 +39,10 @@ const Header = () => {
         ))}
       </ul>
 
+      {/* TAB ITEMS */}
+      {/* 
+      <HeaderDropdown></HeaderDropdown>
+      */}
       <Link href="/">logo</Link>
 
       <div className="flex items-center gap-4">
